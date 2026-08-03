c6058d78b5979694887c3ca5ff9e7498676a333a
Alistair Bishop <alistairbishop@gmx.co.uk>
2026-08-03 21:44:55 +0100

    updated social links

--- src/data/socialLinks.ts
+++ src/data/socialLinks.ts
@@ -1,5 +1,4 @@
 export const socialLinks = [
-  { label: 'GitHub', url: 'https://github.com/alistairbishop06' },
+  { label: 'GitHub', url: 'https://www.github.com/alistairbishop06' },
   { label: 'LinkedIn', url: 'https://www.linkedin.com/in/alistairbishop1' },
-  { label: 'Portfolio', url: 'https://example.com' },
 ];
